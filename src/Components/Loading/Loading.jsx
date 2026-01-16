aa9d108858c31ca27e88175fe3e9d59d775ba914
Sherif Alaa <phantomnogamenolife@gmail.com>
2026-01-16 21:34:00 +0200

    Fix typo in className attribute for Loader2

--- src/Components/Loading/Loading.jsx
+++ src/Components/Loading/Loading.jsx
@@ -1,10 +1,11 @@
+import { Loader2 } from "lucide-react";
 const Loading = () => {
     return (
         <div className='h-screen flex justify-center items-center'>
             <Loader2
                 height="280"
                 width="280"
-                lassName="animate-spin"
+                className="animate-spin"
             />
         </div>
     )
